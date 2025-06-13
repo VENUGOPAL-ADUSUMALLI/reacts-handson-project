9250f725aa68fd0c4a47e517fe84d603883acd73
venu <adusumallivenugopal04@gmail.com>
2025-06-13 22:02:09 +0530

    changes in results page

--- src/coursesPage/ResultsPage.jsx
+++ src/coursesPage/ResultsPage.jsx
@@ -17,7 +17,33 @@ function ResultsPage() {
             <div className='Toppers_container'> 
               <h1 class="Toppers_haeading"> Hear from our 2025 toppers</h1>
             </div>
-            <div> 
+            <div className='results-video-wrapper'> 
+              <div class="video-wrapper">
+                  <video
+                    class="promo-video"
+                    autoplay
+                    muted
+                    loop
+                    playsinline
+                    controls
+                  >
+                  <source src="https://res.cloudinary.com/dpzpn3dkw/video/upload/w_1600,f_auto,q_auto/v1749190791/zrrft8m3t0xzreg74gfq.mp4" type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              </div>
+              <div class="video-wrapper">
+                  <video
+                    class="promo-video"
+                    autoplay
+                    muted
+                    loop
+                    playsinline
+                    controls
+                  >
+                  <source src="https://res.cloudinary.com/dpzpn3dkw/video/upload/w_1600,f_auto,q_auto/v1749190633/kwizrkvh3fixomniapbg.mp4?_upload_ref=ic_img_tool&__ar__=1.78" type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              </div>
               <div class="video-wrapper">
                   <video
                     class="promo-video"
@@ -32,7 +58,24 @@ function ResultsPage() {
                 </video>
               </div>
             </div>
-            
+            <div className='allen-app-advantage-results-image'>
+              <img className='allen-app-advantage-image' src="https://res.cloudinary.com/dpzpn3dkw/image/upload/w_1600,f_avif,q_auto/v1748870427/lpld6cm50rzgjbw9jjrb.png?_upload_ref=ic_img_tool&__ar__=3.66"/>
+            </div>
+            <div className='allen-app-advantage-results-image-online'>
+              <img className='allen-app-advantage-image' src="https://res.cloudinary.com/dpzpn3dkw/image/upload/w_1600,f_avif,q_auto/v1749023702/xwrlmyhrumzsmey8s6rb.png?_upload_ref=ic_img_tool&__ar__=4.86"/>
+            </div>
+            <div >
+              <div className='results-over-the-image-container'> 
+                <h1 className='allen-results-over-the-years-heading'>ALLEN’s JEE results over the years </h1>
+              </div>
+              <div className='results-over-the-image-container'>
+                <img className='results-over-image' src="https://res.cloudinary.com/dpzpn3dkw/image/upload/w_1600,f_avif,q_auto/v1749024828/viwnvb0zpgxrxlgluthc.png?_upload_ref=ic_img_tool&__ar__=3.38"/>
+              </div>
+            </div>
+            <div className='results-button'>
+              <button className='Button-styling-result'> view JEE 2023 results </button>
+              <button className='Button-styling-result'> view JEE 2022 results </button>
+            </div>
            </div>
         </div>
         
